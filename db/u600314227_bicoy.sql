2a6f5a062b490543f5abcc9fa3cbc35257ae6340
RamVQ <rvq25@hotmail.com>
2017-06-01 23:03:59 -0600

    selenium  login tests

--- db/u600314227_bicoy.sql
+++ db/u600314227_bicoy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-05-2017 a las 04:38:10
+-- Tiempo de generación: 02-06-2017 a las 06:57:06
 -- Versión del servidor: 10.1.22-MariaDB
 -- Versión de PHP: 7.1.4
 
@@ -3274,10 +3274,10 @@ INSERT INTO `users` (`RowID`, `UserID`, `PassHash`, `Forename`, `MiddleName`, `F
 (2, 540296096, '11a80318e06948432cfeaa32053ec0604612d7a01beb5537b88e9e29c420934c', 'Sharon', 'Sharon', 'Jimenez', 'Quesada', 'Jimenez@bac san jose.com', 85328719, 'technician', 'bac san jose', 'FALSE', '2017-06-28 00:00:00', 119719940, '183.180.128.3', ''),
 (3, 435037716, '6c656245d7db91909f24e22eca56593e267efe475430ce94763e97c192f83f7d', 'Erick', 'Nicole', 'Chaverria', 'Rodriguez', 'Chaverria@boston scientific.com', 85440604, 'viewer', 'boston scientific', 'TRUE', '2017-09-15 00:00:00', 540296096, '243.124.121.16', ''),
 (4, 616433553, '0dfbdb6148171b48b06f4d829a5c753ba27b97a6d385bafd30a605cacb117ba8', 'Ramiro', 'Nicole', 'Araya', 'Alvarez', 'Araya@hpe.com', 89163665, 'none', 'hpe', 'FALSE', '2016-04-12 00:00:00', 129438171, '149.138.159.6', ''),
-(5, 444888697, '648306867598c98b2d6f54b0ed3a110b38fbc386ac7b6d47b64385f6a5a249ce', 'Maria', 'Sofia', 'Salas', 'Alfaro', 'Salas@hpe.com', 84964846, 'guess', 'hpe', 'FALSE', '2017-06-24 00:00:00', 119719940, '152.118.158.6', ''),
+(5, 444888697, '648306867598c98b2d6f54b0ed3a110b38fbc386ac7b6d47b64385f6a5a249ce', 'Maria', 'Sofia', 'Salas', 'Alfaro', 'Salas@hpe.com', 84964846, 'guest', 'hpe', 'FALSE', '2017-06-02 03:29:11', 119719940, '152.118.158.6', ''),
 (6, 459779329, '7e486a43eb58adf4ba770435d4ec212aca62e9bf869b19be2f239c59c7600123', 'Catalina', 'Sharon', 'Mora', 'Alvarez', 'Mora@bac san jose.com', 88460476, 'technician', 'bac san jose', 'FALSE', '2015-01-18 00:00:00', 730300606, '161.156.179.21', ''),
 (8, 119719940, '67a44257327a4df319099641a325cb97ad93055bb511f3c3e6484d5ffc783568', 'Fernanda', 'Erick', 'Araya', 'Gonzalez', 'Araya@boston scientific.com', 81182754, 'viewer', 'boston scientific', 'TRUE', '2017-09-17 00:00:00', 540296096, '208.182.127.15', ''),
-(9, 270049781, 'ef1e841b49ed01280c0380375269d25855ef1be8553179f7c4c5c428c5666c58', 'Maria', 'Ramiro', 'Rodriguez', 'Mora', 'Rodriguez@banco nacional.com', 87277913, 'admin', 'banco nacional', 'TRUE', '2015-09-08 00:00:00', 444888697, '210.152.125.30', ''),
+(9, 270049781, 'ef1e841b49ed01280c0380375269d25855ef1be8553179f7c4c5c428c5666c58', 'Maria', 'Ramiro', 'Rodriguez', 'Mora', 'Rodriguez@banco nacional.com', 87277913, 'administrator', 'banco nacional', 'TRUE', '2017-06-02 04:28:55', 444888697, '210.152.125.30', ''),
 (14, 8211524631, '2ff52f1446a801cd680dbd56157d7e3bc598076c1fae8f0495ed11c8b801041b', 'esteban', 'allan', 'alfaro', 'salas', 'undefined', 0, 'administrator', 'NONE', 'inactive', '2017-03-06 12:25:26', 0, '', 'false'),
 (15, 8211524631, 'NONE', 'esteban', 'allan', 'alfaro', 'salas', 'undefined', 0, 'administrator', 'NONE', 'inactive', '2017-03-06 12:25:33', 0, '', 'false'),
 (16, 8211524631, 'NONE', 'esteban', 'allan', 'alfaro', 'salas', 'undefined', 0, 'administrator', 'NONE', 'inactive', '2017-03-06 12:25:29', 0, '', 'false'),
@@ -3289,8 +3289,8 @@ INSERT INTO `users` (`RowID`, `UserID`, `PassHash`, `Forename`, `MiddleName`, `F
 (21, 111610825, 'NONE', 'javier', 'NONE', 'gonzalez', 'murillo', 'undefined', 0, 'Guest', 'FireINC', 'inactive', '2017-03-07 03:00:54', 0, '', 'false'),
 (22, 99881225, 'NONE', 'mariano', 'NONE', 'velez', 'cardona', 'undefined', 0, 'Viewer', 'FireINC', 'active', '2017-03-13 00:28:03', 0, '', 'false'),
 (23, 131937220, 'e622511df8bf6d63c1b057f9340af6619be22db0c79c9017a1d0c7a11e833295', 'Jose', 'Harold', 'Jimenez', 'Quesada', 'Jimenez@bac san jose.com', 87445492, 'developer', 'bac san jose', 'TRUE', '2017-01-14 00:00:00', 616433553, '211.151.205.6', ''),
-(7, 129438171, '6117277', 'Fernanda', 'Sharon', 'Quesada', 'Gonzalez', 'Quesada@bac san jose.com', 87547639, 'admin', 'bac san jose', 'TRUE', '2015-04-21 00:00:00', 459779329, '198.164.222.29', ''),
-(10, 362295981, '5513947', 'Jorge', 'Nicole', 'Chaverria', 'Rodriguez', 'Chaverria@bac san jose.com', 89533296, 'technician', 'bac san jose', 'FALSE', '2016-08-07 00:00:00', 129438171, '190.120.166.4', ''),
+(7, 129438171, 'ee3edd95e8bc36c50bfd9b975c785d894e54047ffda62ae86d010959a0c2895c', 'Fernanda', 'Sharon', 'Quesada', 'Gonzalez', 'Quesada@bac san jose.com', 87547639, 'owner', 'bac san jose', 'TRUE', '2017-06-02 04:36:12', 459779329, '198.164.222.29', ''),
+(10, 362295981, '8d1ac43b6b164223da1e0234248668bb5ecacc721ba2061d8d0245b647590bd4', 'Jorge', 'Nicole', 'Chaverria', 'Rodriguez', 'Chaverria@bac san jose.com', 89533296, 'technician', 'bac san jose', 'FALSE', '2017-06-02 04:49:01', 129438171, '190.120.166.4', ''),
 (11, 345100213, '5256865', 'Fernanda', 'Erick', 'Alfaro', 'Quesada', 'Alfaro@bac san jose.com', 87183572, 'editor', 'bac san jose', 'TRUE', '2015-03-25 00:00:00', 444888697, '176.202.148.4', ''),
 (12, 594929479, '215fb2c827737c3b1a13d66eed4055bd870f0314c298bd461f92a83a5976ff37', 'Maria', 'Ana', 'Quesada', 'Mora', 'Quesada@cfia.com', 82594518, 'editor', 'cfia', 'FALSE', '2017-04-27 00:00:00', 540296096, '161.167.210.12', ''),
 (24, 111610819, 'f226724bac26b888d6fa223c50c57ed0fbf85e93afd747e205116fc454ab872b', '', NULL, '', '', '', 0, 'administrator', '', 'inactive', '2017-04-12 02:11:21', 0, '', 'false'),
